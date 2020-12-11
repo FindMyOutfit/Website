65855747105b4ea12e9233a8951796f1f5d57599
Nimay-1709 <60466954+Nimay-1709@users.noreply.github.com>
2020-12-10 23:05:54 -0800

    Update Admin_Main_Page.php

--- Admin_Main_Page.php
+++ Admin_Main_Page.php
@@ -15,35 +15,32 @@ include_once 'user-session.php';
   <link rel="stylesheet" type="text/css" href="sidebar.css">
   <link rel="stylesheet" type="text/css" href="common.css">
 
-  <!-- fontawesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 
 <body>
-  <div class="layout"> <a class="header" href="#0"> <i class="fa fa-bars"></i>
-  <div class="header-user"><i class="fas fa-user-circle icon"></i>Hello Admin</div>
+  <div class="layout"> <a class="header" href="#0"> <i class="fa fa-bars"></i><br>
+  <div class="header-user"><i class="fas fa-user-circle icon" style="padding-right: 40px"></i>Hello Admin</div>
     </a>
-
+  <form name="form" method="post">
     <div class="sidebar">
       <ul>
         <li> <a class="sidebar-list-item active" href="#0"> <i class="fas fa-home icon"></i><em>Dashboard</em></a></li>
-        <li> <a class="sidebar-list-item" href="#0"> <i class="fas fa-user icon"></i><em>Remove Profiles</em></a></li>
-        <li> <a class="sidebar-list-item" href="#0"> <i class="fas fa-tasks icon"></i><em>Eligibility Applications</em></a></li>
+        <li> <a class="sidebar-list-item" href="Remove_Profile.html"> <i class="fas fa-user icon"></i><em>Remove Profiles</em></a></li>
+        <li> <a class="sidebar-list-item" href="Eligiblity.html"> <i class="fas fa-tasks icon"></i><em>Eligibility Applications</em></a></li>
         <li> <a class="sidebar-list-item" href="Backup.html"> <i class="fas fa-calendar icon"></i><em>Backup Credentials</em></a>
         </li>
-        <li> <a class="sidebar-list-item" href="#0"> <i class="fas fa-toolbox icon"></i><em>Compensation</em></a>
+        <li> <a class="sidebar-list-item" href="Compensation.html"> <i class="fas fa-toolbox icon"></i><em>Compensation</em></a>
         </li>
-        <li> <a class="sidebar-list-item" href="#0"> <i class="fas fa-comments icon"></i><em>User Complaints</em></a></li>
-        <li> <a class="sidebar-list-item" href="#0"> <i class="fas fa-map icon"></i><em>View Locations</em></a>
+        <li> <a class="sidebar-list-item" href="Complains.html"> <i class="fas fa-comments icon"></i><em>User Complaints</em></a></li>
         </li>
       </ul>
     </div>
-
     <footer class="footer">
-      <div class="footer_sign"><input type="submit" name="outbutton" class="button" href="login.html" value="Logout">
+      <div class="footer_sign"><input type="submit" name="outbutton" class="button" value="Logout">
       </div>
     </footer>
-    <?php
+<?php
       if(isset($_POST['outbutton']))
       {
         session_unset();
