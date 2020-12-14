b1ec85eb28c37ef12b251928eb60e3bc516304d4
Nimay-1709 <60466954+Nimay-1709@users.noreply.github.com>
2020-12-13 19:44:20 -0800

    Update Backup.php

--- Backup.php
+++ Backup.php
@@ -1,13 +1,33 @@
 <?php
 include_once 'user-session.php';
+include_once 'database-connect.php';
+include_once 'validations.php';
       if(!isset($_SESSION['username']))
-        header("Location: login.php");
+        header("location: login.php");
 
-      if(isset($_POST['submit']))
-         {
-           header('location: Admin_Main_Page.php');
-         }
-   ?>
+      if(isset($_POST['Savebutton']))
+      {
+          $error = array();
+          $required = array('username', 'password');
+          $error = array_merge($error, check_empty_field($required));
+          $backup = $_POST['backup'];
+          if(empty($error))
+          {
+              $emailid = $_POST['username'];
+              $SQLQuery = "INSERT INTO admin (email, backup) VALUES ('$emailid','$backup',)";
+              $statement = $db->prepare($SQLQuery);
+              $statement->execute();
+          }
+          else{
+              if(count($error) == 1){
+                  $result = "<p style='color:red; text-align:center;'>There was 1 error in the form</p>";
+              }
+              else{
+                  $result = "<p style='color:red; text-align:center;'>There were " .count($error). " error in your submission</p>";
+              }
+          }
+          header("location: Admin_Main_Page.php");
+      }
 ?>
 <html>
 <head>
@@ -22,22 +42,17 @@ include_once 'user-session.php';
 <link rel="stylesheet" type="text/css" href="sidebar.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
-<body>
   <div class="layout"> <a class="header" href="#0"> <i class="fa fa-bars"></i><br>
 
   <div class="header-user"><i class="fas fa-user-circle icon" style="padding-right: 40px"></i>Hello, Admin</div>
     </a>
-    <form name="form" method="post">
+    <form name="back_form" method="post">
   <div class="sidebar">
     <ul>
       <li> <a class="sidebar-list-item" href="Admin_Main_Page.php"> <i class="fas fa-home icon"></i><em>Dashboard</em></a></li>
       <li> <a class="sidebar-list-item" href="Remove_Profile.php"> <i class="fas fa-user icon"></i><em>Remove Profiles</em></a></li>
       <li> <a class="sidebar-list-item" href="Eligibility.php"> <i class="fas fa-tasks icon"></i><em>Eligibility Applications</em></a></li>
       <li> <a class="sidebar-list-item active" href="#0"> <i class="fas fa-calendar icon"></i><em>Backup Credentials</em></a>
-      </li>
-      <li> <a class="sidebar-list-item" href="Compensation.php"> <i class="fas fa-toolbox icon"></i><em>Compensation</em></a>
-      </li>
-      <li> <a class="sidebar-list-item" href="Complains.php"> <i class="fas fa-comments icon"></i><em>User Complaints</em></a></li>
       </li>
     </ul>
   </div>
@@ -48,34 +63,35 @@ include_once 'user-session.php';
       <h1>Establish Second Email</h1>
     </div>
     <div class="main-form">
-      <form name="event">
-        <input type="text" name="ftitle" placeholder="Enter Default Email"><br>
-        <input type="text" name="fdescription" placeholder="Enter Password"><br>
-        <input type="text" name="flocation" placeholder="Enter Backup Email"><br><br><br>
-
-        <label for="fallday" class="chechbox">Check to replace the Default :
-          <input type="checkbox" class="checkbox" name="replace" onchange="form_display_hour()">
+      <form name="event" method="post">
+        <input type="text" name="username" placeholder="Enter Default Email"><br>
+        <input type="text" name="password" placeholder="Enter Password"><br>
+        <input type="text" name="backup" placeholder="Enter Backup Email"><br><br><br>
+        <label for="fallday" class="chechbox">Check to confirm changes :
+          <input type="checkbox" class="checkbox" name="replace">
         </label>
-
-        <input type="submit" name="submit" value="Save" class="button">
-      </form>
+        <div>
+           <input type="submit" name="Savebutton" class="button" value="Add Backup"><br>
+         </div>
+        </form>
     </div>
-  </div>
+</div>
 </main>
-<footer class="footer">
-    <div class="container">
-      <p>Made with <span class="fas fa-heart"></span> by Prof. Ishie Eswar's Students</p>
-    </div>
-  <div class="footer_sign"><input type="submit" name="outbutton" class="button" value="Logout">
+<footer id="footer" class="static-footer">
+  <div class="container">
+    <p>Made with <span class="fas fa-heart"></span> by Prof. Ishie Eswar's Students</p>
   </div>
+<div class="footer_sign"><input type="submit" name="outbutton" class="button" value="Logout">
+</div>
 </footer>
 <?php
-     if(isset($_POST['outbutton']))
-      {
-        session_unset();
-        session_destroy();
-        header('location: login.php');
-      }
+   if(isset($_POST['outbutton']))
+    {
+      session_unset();
+      session_destroy();
+      header('location: login.php');
+    }
 ?>
+</div>
 </body>
 </html>
