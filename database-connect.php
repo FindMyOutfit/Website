d96f8e2a6f3cacfc6237fc599c5606b9805b612c
Nimay-1709 <60466954+Nimay-1709@users.noreply.github.com>
2020-12-13 19:49:02 -0800

    Update database-connect.php

--- database-connect.php
+++ database-connect.php
@@ -4,15 +4,14 @@ $username = 'root';
 $password = '';
 $dsn = 'mysql:host=localhost; dbname=findmyoutfit';
 
-try {
-
+try 
+{
  $db = new PDO($dsn, $username, $password);
  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-
+} 
+catch (PDOException $e)
+{
  echo "Fail to connect to the database ".$e->getMessage();
-
 }
 
 ?>
