d767564bdd87a8acbe008fa91760061324ae45ea
jaysolankijay <74154830+jaysolankijay@users.noreply.github.com>
2020-12-14 09:24:01 -0800

    Update User_Main_Page.php

--- User_Main_Page.php
+++ User_Main_Page.php
@@ -4,12 +4,14 @@ include_once 'user-session.php';
         header("Location: login.php");
 ?>
 
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find My Outfit</title>
     <link href = "dashstyle.css" rel = "stylesheet" type = "text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
 <body>
     <header>
@@ -21,14 +23,14 @@ include_once 'user-session.php';
                 <li><input type="submit" name="logoutbutton" class="button" value="Logout"></li>
             </ul>
         </div>
-         <?php
+        <?php
          if(isset($_POST['logoutbutton']))
           {
             session_unset();
             session_destroy();
             header('location: login.php');
           }
-    ?>
+        ?>
     </header>
     <div class="buttons">
         <ul>
@@ -38,5 +40,10 @@ include_once 'user-session.php';
              <li><a href="#">Payment Details</a></li>
         </ul>
     </div>
+    <footer id="footer" class="static-footer">
+      <div class="container">
+        <p>Made with <span class="fas fa-heart"></span> by Prof. Ishie Eswar's Students</p>
+      </div>
+    </footer>
 </body>
 </html>
