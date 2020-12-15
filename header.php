ad2e2a78a78b7c7b39f9b6dc76fffe07c23e8353
Sarthak Madrecha <59553910+sarthak-3112@users.noreply.github.com>
2020-12-15 21:57:22 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -3,32 +3,32 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title>ADMIN PANEL</title>
-    <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="css/text.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="css/grid.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="css/layout.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="css/nav.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="reset.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="text.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="grid.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="layout1.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="nav.css" media="screen" />
     <!--[if IE 6]><link rel="stylesheet" type="text/css" href="css/ie6.css" media="screen" /><![endif]-->
     <!--[if IE 7]><link rel="stylesheet" type="text/css" href="css/ie.css" media="screen" /><![endif]-->
     <!-- BEGIN: load jquery -->
     <script src="js/jquery-1.6.4.min.js" type="text/javascript"></script>
-    <script type="text/javascript" src="js/jquery-ui/jquery.ui.core.min.js"></script>
-    <script src="js/jquery-ui/jquery.ui.widget.min.js" type="text/javascript"></script>
-    <script src="js/jquery-ui/jquery.ui.accordion.min.js" type="text/javascript"></script>
-    <script src="js/jquery-ui/jquery.effects.core.min.js" type="text/javascript"></script>
-    <script src="js/jquery-ui/jquery.effects.slide.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="jquery.ui.core.min.js"></script>
+    <script src="jquery.ui.widget.min.js" type="text/javascript"></script>
+    <script src="jquery.ui.accordion.min.js" type="text/javascript"></script>
+    <script src="jquery.effects.core.min.js" type="text/javascript"></script>
+    <script src="jquery.effects.slide.min.js" type="text/javascript"></script>
     <!-- END: load jquery -->
     <!-- BEGIN: load jqplot -->
-    <link rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css" />
+    <link rel="stylesheet" type="text/css" href="jquery.jqplot.min.css" />
     <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="js/jqPlot/excanvas.min.js"></script><![endif]-->
-    <script language="javascript" type="text/javascript" src="js/jqPlot/jquery.jqplot.min.js"></script>
-    <script language="javascript" type="text/javascript" src="js/jqPlot/plugins/jqplot.barRenderer.min.js"></script>
-    <script language="javascript" type="text/javascript" src="js/jqPlot/plugins/jqplot.pieRenderer.min.js"></script>
-    <script language="javascript" type="text/javascript" src="js/jqPlot/plugins/jqplot.categoryAxisRenderer.min.js"></script>
-    <script language="javascript" type="text/javascript" src="js/jqPlot/plugins/jqplot.highlighter.min.js"></script>
-    <script language="javascript" type="text/javascript" src="js/jqPlot/plugins/jqplot.pointLabels.min.js"></script>
+    <script language="javascript" type="text/javascript" src="jquery.jqplot.min.js"></script>
+    <script language="javascript" type="text/javascript" src="jqplot.barRenderer.min.js"></script>
+    <script language="javascript" type="text/javascript" src="jqplot.pieRenderer.min.js"></script>
+    <script language="javascript" type="text/javascript" src="jqplot.categoryAxisRenderer.min.js"></script>
+    <script language="javascript" type="text/javascript" src="jqplot.highlighter.min.js"></script>
+    <script language="javascript" type="text/javascript" src="jqplot.pointLabels.min.js"></script>
     <!-- END: load jqplot -->
-    <script src="js/setup.js" type="text/javascript"></script>
+    <script src="setup.js" type="text/javascript"></script>
     <script type="text/javascript">
 
         $(document).ready(function () {
@@ -49,12 +49,9 @@
 
                     </div>
                 <div class="floatright">
-                    <div class="floatleft">
-                        </div>
                     <div class="floatleft marginleft10">
                         <ul class="inline-ul floatleft">
-                            
-                            
+                                                      
                             <li><a href="logout.php">Logout</a></li>
                         </ul>
                         <br />
@@ -70,8 +67,7 @@
         <div class="grid_12">
             <ul class="nav main">
                 <li ><a href="addcart.php"><span>Add Product</span></a> </li>
-                <li ><a href="removecart.php"><span>Remove Product</span></a> </li>                
-                <li ><a href="typography.html"><span>Confirm Orders</span></a></li>
+               <li ><a href="typography.html"><span>Confirm Orders</span></a></li>
 				<li ><a href="charts.html"><span>Update History</span></a></li>
 
 
